bbebd69250caca05ca2dcf9f7c50440393621c2f
Simon Weiler <simonw@ad.swc.ucl.ac.uk>
2022-02-18 23:41:24 +0000

    updated time to peak using gaussian smoothing

--- time_to_peak.m
+++ time_to_peak.m
@@ -1,22 +1,31 @@
-function [time_tp]=time_to_peak(trac,bs,act,sr,cc)
+function [time_tp trace_smooth]=time_to_peak(trac,bs,act,sr,cc,fc)
 
-st_bs=3.5*std(diff(trac(bs)));
+%st_bs=4*std(diff(trac(bs)));
+filter_used='gaussian';
+bs_diff=smoothdata(diff(trac(bs)),filter_used);
+act_diff=smoothdata(diff(trac(act)),filter_used);
+st_bs=fc*std(bs_diff);
+trace_smooth=trac(act);
+
 if cc==0
-temp=find(diff(trac(act))<-st_bs);
-figure;plot(diff(trac(act)));hold on;plot([0 100],[-st_bs -st_bs],'--k');
+%temp=find(diff(trac(act))<-st_bs);
+temp=find(act_diff<-st_bs);
+figure;plot(act_diff);hold on;plot([0 1000],[-st_bs -st_bs],'--k');
 if isempty(temp)==0
-time_tp=temp(1);
+time_tp=temp(1)/sr;
 else
     time_tp=NaN;
 end
 
 else cc==1
-temp=find(diff(trac(act))>st_bs);
-figure;plot(diff(trac(act)));hold on;plot([0 100],[st_bs st_bs],'--k');
+%temp=find(diff(trac(act))>st_bs);
+temp=find(act_diff>st_bs);
+figure;plot(act_diff);hold on;plot([0 1000],[-st_bs -st_bs],'--k');
 if isempty(temp)==0
-time_tp=temp(1);
+time_tp=temp(1)/sr;
 else
     time_tp=NaN;
 end
+
 end
 end
